7dd41187846fb1cd151e9d5eefa44b96f9e098e1
dicke <dicke.jonas@gmail.com>
2025-10-26 13:03:34 +0100

    initial commit

---
+++ assignment01/assignment.cc
@@ -0,0 +1,18 @@
+/*===========================================================================*/
+/*                                                                           *
+ * Copyright (c) 2020, Computer Graphics Group RWTH Aachen University        *
+ *                            All rights reserved                            *
+ *                                                                           *
+ * Basic Techniques in Computer Graphics Exercise                            *
+ *                                                                           */
+/*===========================================================================*/
+
+#include "assignment.hh"
+
+void task::drawScene(int scene, float runTime) {
+    std::cout << scene << " " << runTime << std::endl;
+}
+
+void task::initCustomResources() {}
+
+void task::deleteCustomResources() {}
---
+++ build/extern/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dicke/Downloads/CG/basic-techniques/extern/glm/glm
+# Build directory: /home/dicke/Downloads/CG/basic-techniques/build/extern/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

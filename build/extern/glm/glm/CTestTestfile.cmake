8ef956226f6e8f431554c2675b3c20c6305d9d52
dicke <dicke.jonas@gmail.com>
2025-10-26 13:22:48 +0100

    remobe build/

--- build/extern/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dicke/Downloads/CG/basic-techniques/extern/glm/glm
-# Build directory: /home/dicke/Downloads/CG/basic-techniques/build/extern/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
